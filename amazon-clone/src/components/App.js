a6543bd9fd57dc300693c0c01c2a5b6cbfaf1dbe
Siddharth Manimaran <siddharthmaaran@gmail.com>
2021-09-04 11:41:32 +0530

    setting headwer

--- amazon-clone/src/components/App.js
+++ amazon-clone/src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
-  return <h1>Hello World!!!!</h1>;
+  return (
+    <div>
+      <Header />
+      <h1>Hello World!!!!</h1>
+    </div>
+  );
 }
 
 export default App;
