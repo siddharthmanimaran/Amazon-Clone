ffe92d38dc4f6bd2efa8122d9e05ff2d887f1fc2
Siddharth Manimaran <siddharthmaaran@gmail.com>
2021-09-09 14:10:40 +0530

    settings product in backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import data from "./data.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.get("/", (req, res) => {
+  res.send("server is started");
+});
+app.listen(port, () => {
+  console.log(`server at port  ${port}`);
+});
